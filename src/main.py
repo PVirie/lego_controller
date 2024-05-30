f9b056f255d3dd117b12ca3a3982c4959e2535f4
Patrick Virie <p.virie@gmail.com>
2024-05-30 21:30:15 +0700

    use keyboard

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import lego_controller as lc
 import signal
 from datetime import datetime, timedelta
 import random
+import keyboard
 
 async def main():
     buwizz_interface = None
@@ -25,34 +26,68 @@ async def main():
     await buwizz_interface.start()
     await buwizz_interface.set_data_refresh_rate(100)
     
-    await buwizz_interface.set_powerup_motor_mode(lc.Buwizz_3_Ports.PORT_1, lc.Buwizz_3_PU_Modes.POSITION, 0)
-    await buwizz_interface.set_powerup_motor_mode(lc.Buwizz_3_Ports.PORT_2, lc.Buwizz_3_PU_Modes.POSITION, -90)
+    await buwizz_interface.set_powerup_motor_mode(lc.Buwizz_3_Ports.PORT_3, lc.Buwizz_3_PU_Modes.POSITION, 0)
+    await buwizz_interface.set_powerup_motor_mode(lc.Buwizz_3_Ports.PORT_4, lc.Buwizz_3_PU_Modes.POSITION, -90)
 
     start_time = datetime.now()
     while True:
         now_time = datetime.now()
 
         status = await buwizz_interface.get_status()
-        print(status)
+        print(status, end='\r')
 
-        # Power up servo modes
-        await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_1, 1)
-        await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_2, -1)
-
-        # Power up speed modes
-        await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_3, 1)
-        await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_4, -1)
+        # write arrow keys to control the motors
+        if keyboard.is_pressed("q"):
+            await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_1, 1)
+        elif keyboard.is_pressed("a"):
+            await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_1, -1)
+        else:
+            await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_1, 0)
         
-        # Power function servo modes
-        await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_A, -90)
+        if keyboard.is_pressed("e"):
+            await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_2, 1)
+        elif keyboard.is_pressed("d"):
+            await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_2, -1)
+        else:
+            await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_2, 0)
 
-        # Power function speed modes
-        await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_B, 1)
+
+        if keyboard.is_pressed("up"):
+            await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_3, 90)
+        elif keyboard.is_pressed("down"):
+            await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_3, -90)
+        else:
+            await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_3, 0)
+
+        if keyboard.is_pressed("right"):
+            await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_4, 90)
+        elif keyboard.is_pressed("left"):
+            await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_4, -90)
+        else:
+            await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_4, 0)
+
+        # # Power up servo modes
+        # await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_1, 90)
+        # await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_2, 180)
+
+        # # Power up speed modes
+        # await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_3, 1)
+        # await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_4, -1)
+        
+        # # Power function servo modes
+        # await buwizz_interface.set_motor_angle(lc.Buwizz_3_Ports.PORT_A, -90)
+
+        # # Power function speed modes
+        # await buwizz_interface.set_motor_velocity(lc.Buwizz_3_Ports.PORT_B, 1)
 
 
-        if now_time - start_time > timedelta(seconds=10):
-            # await buwizz_interface.activate_shelf_mode()
+        if keyboard.is_pressed('esc'):
             await buwizz_interface.exit()
+            break
+
+        if keyboard.is_pressed('f12'):
+            await buwizz_interface.activate_shelf_mode()
+            print('Shelf mode activated')
             break
 
 
